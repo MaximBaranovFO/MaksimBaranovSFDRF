8f92620c1f854eedba6381a04e58f1176a6917fb
MaximBaranovFO <91403309+MaximBaranovFO@users.noreply.github.com>
2022-03-18 15:42:45 +0700

    +1
    +1

---
+++ src/pathCmdJava/createJar202203181340.bat
@@ -0,0 +1,6 @@
+cd c:\_bmv\17JavaLearn\books\cmdIntoJFrame\
+"C:\Program Files\Java\jdk-17.0.2\bin\javac.exe" ExeOutputFrame.java
+"C:\Program Files\Java\jdk-17.0.2\bin\jar.exe" cvfm ExeOutputFrame.jar manifest.txt ExeOutputFrame.class
+"C:\Program Files\Java\jdk-17.0.2\bin\jar.exe" cvfe ExeOutputFrame.jar ExeOutputFrame *.class
+"C:\Program Files\Java\jdk-17.0.2\bin\java.exe" -jar ExeOutputFrame.jar
+cd C:\_bmv\13cmdLearn\
