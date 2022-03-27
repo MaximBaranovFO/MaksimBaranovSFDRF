831ad6d60e2d77fb7b4ddd3a472245d92e9d688f
MaximBaranovFO <91403309+MaximBaranovFO@users.noreply.github.com>
2022-03-27 18:51:14 +0700

    +1
    +1

--- src/pathJava17/gradleI001MBSFDRF/desktop/src/main/java/gradleI001MBSFDRF/RunCmdThExecInterface003.java
+++ src/pathJava17/gradleI001MBSFDRF/desktop/src/main/java/gradleI001MBSFDRF/RunCmdThExecInterface003.java
@@ -9,6 +9,17 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Администратор
@@ -22,7 +33,18 @@ public interface RunCmdThExecInterface003 {
    
    public static void mainWhoRunThForCmd()
    {
-       ForRunThreadsMaker.PausableThreadPoolExecutor newElementPausableThreadPool = ForRunThreadsMaker.PausableThreadPoolExecutor.getNewElement(5);
+       fromThreadRunJavaFXwindow();
+       
+   }
+   private static void fromThreadRunJavaFXwindow(){
+       Thread whoRunThat = new Thread(){
+        @Override
+        public void run(){
+            runJavaFXwindow();
+            
+            
+            
+            ForRunThreadsMaker.PausableThreadPoolExecutor newElementPausableThreadPool = ForRunThreadsMaker.PausableThreadPoolExecutor.getNewElement(5);
        countNewElement = 1;
        if( countNewElement > 1 ){
            newElementPausableThreadPool.beforeExecute(new Thread(), new ElementsRunInThisVal());
@@ -32,6 +54,14 @@ public interface RunCmdThExecInterface003 {
            RunCmdThExecInterface001.ThreadsPerExecutorService.mainWhoRunThForCmd();
        if( countNewElement > 1 )
            RunCmdThExecInterface002.ExeOutputFrameAnotherVersion002.mainOutExecIntoFrame();
+        };
+       };
+   }
+   private static void runJavaFXwindow(){
+       CompanyOfWindow fxApp = new CompanyOfWindow();
+       String[] valInto = {"", ""};
+       fxApp.mainCompanyOfWindow(valInto);
+       //fxApp.stop();
    }
    private static void runCmdShell(){
         try {
@@ -100,4 +130,53 @@ public interface RunCmdThExecInterface003 {
         }
         
         }
+        public class User{
+            private String nameUser = new String("");
+            private Integer ageUser = 0;
+            private Integer moneyCountUser = 0;
+            private Boolean isExist;
+            
+        User(String nameUserInputed, 
+                Integer ageUserInputed,
+                Integer moneyCountUser, 
+                Boolean isExist){
+        }
+        }
+        public class CompanyOfWindow extends Application {
+public void start(Stage primaryStage) throws Exception {
+users.add(new User("Alice", 20, 150, false));
+users.add(new User("Bob", 34, 300, true));
+users.add(new User("Peter", 18, 100, false));
+users.add(new User("Kate", 38, 300, true));
+users.add(new User("Steve", 31, 250, true));
+users.add(new User("Alan", 62, 500, true));
+users.add(new User("Julia", 33, 320, true));
+users.add(new User("Patric", 37, 300, true));
+users.add(new User("Alexander", 34, 280, true));
+users.add(new User("George", 28, 180, true));
+users.add(new User("Mary", 22, 190, false));
+userBox.getItems().addAll(users);
+root.getChildren().add(strings);
+strings.setPadding(new Insets(10, 30, 10, 30));
+strings.setSpacing(20);
+strings.getChildren().add(new Text("Select the user"));
+strings.getChildren().add(buttonBox);
+buttonBox.setSpacing(10);
+buttonBox.getChildren().add(userBox);
+Scene scene = new Scene(root, WIDTH, HEIGHT);
+primaryStage.setTitle("Company");
+primaryStage.setScene(scene);
+primaryStage.show();
+}
+public static void mainCompanyOfWindow(String[] args) {
+launch(args);
+}
+Group root = new Group();
+VBox strings = new VBox();
+HBox buttonBox = new HBox();
+ComboBox<User> userBox = new ComboBox<>();
+final private int WIDTH = 1000;
+final private int HEIGHT = 600;
+private ArrayList<User> users = new ArrayList<>();
+}
 }
